05b9713537aaf42065d7b803a0e108699c28e21e
dcerbero <dcerbero@proton.me>
2025-04-18 11:51:27 -0500

    add resource

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus2"
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
